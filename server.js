d9b2a010f2c9483cae78e23d17e947b2d9d7f185
“Wes <wesleyrwhite86@gmail.com>
2019-07-30 20:34:28 -0700

    Created new branch, server listening on port 4000 and created logger middleware function

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
 
+server.use(logger);
+server.use(express.json());
+
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
-};
+  const date = new Date();
+  console.log(`${req.method} to ${req.path} at`, date);
+  next();
+}
 
 module.exports = server;
