e60f11a0a672d7448cdf6caad410fc3273d4e7bc
“Wes <wesleyrwhite86@gmail.com>
2019-08-01 17:01:21 -0700

    Completed postRouter file

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -2,26 +2,60 @@ const express = 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+const db = require('./postDb');
 
+router.get('/', validatePost, (req, res) => {
+  const posts = await db.get();
+  res.status(200).json(posts);
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validatePostId, (req, res) => {
+  res.status(200).json(req.validId);
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', validatePostId, (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletePost = await db.remove(id);
+        res.status(200).json(deletePost);
+    } catch(error){
+        res.status(500).json({success: false, message: "We were unable to remove your post." })
+    }
 });
 
-router.put('/:id', (req, res) => {
-
+router.put('/:id', validatePostId, (req, res) => {
+    try{
+        const {id} = req.params
+        const edit = await db.update(id, req.body)
+        res.status(200).json(edit)
+    } catch(error){
+        res.status(500).json({success: false, message:"We were unable to update your post."})
+    }
 });
 
 // custom middleware
 
-function validatePostId(req, res, next) {
+async function validatePost(req, res, next) {
+  if (!req.body) {
+    res.status(400).json({ success: false, message: 'Missing post data' });
+    next();
+  } else if (!req.body.text) {
+    res
+      .status(400)
+      .json({ success: false, message: 'Missing required text field' });
+    next();
+  }
+} 
 
-};
+async function validatePostId(req, res, next) {
+  const { id } = req.params;
+  const validId = await posts.getById(id);
+  if (validId) {
+    req.validId = validId;
+    next();
+  } else {
+    res.status(404).json({ success: false, message: 'Invalid user id' });
+  }
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const userRouter = require('./users/userRouter');
+const postRouter = require('./posts/postRouter');
 
 const server = express();
 
 server.use(logger);
 server.use(express.json());
+server.use(postRouter);
+server.use(userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
